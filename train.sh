e8207170336fe5fd6884ccdb0ae88fa02943248d
andy <experiecore@gmail.com>
2020-06-13 17:10:08 +0800

    make train, eval, test files

--- train.sh
+++ train.sh
@@ -1,11 +1,12 @@
-export TRAIN_FILE=/path/to/dataset/wiki.train.raw
-export TEST_FILE=/path/to/dataset/wiki.test.raw
+export TRAIN_FILE=train.txt
+export TEST_FILE=test.txt
 
 python run_language_modeling.py \
     --output_dir=output \
     --model_type=gpt2 \
-    --model_name_or_path=gpt2 \
+    --model_name_or_path=openai-gpt \
     --do_train \
+    --overwrite_output_dir \
     --train_data_file=$TRAIN_FILE \
     --do_eval \
     --eval_data_file=$TEST_FILE
